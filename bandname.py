685de177135c43d807bff8287dfec38f57d719f0
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2022-12-01 13:54:59 +0530

    Add files via upload

---
+++ bandname.py
@@ -0,0 +1,5 @@
+#band name generator project-1
+print("Welcome to Band name Generator")
+street= input("which place you grew up in?\n")
+pet=input("what is your pet animal?\n")
+print("Your band name could be "+street+" "+pet)
